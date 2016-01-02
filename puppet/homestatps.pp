0297626e38f54fcd28832b6f89cd2f5ae7bfa55f
Ivan Bahdanau <ibahdanau@fitbit.com>
2016-01-02 10:31:54 -0800

    Commenting out everything except apt-get update.

--- puppet/homestatps.pp
+++ puppet/homestatps.pp
@@ -4,30 +4,30 @@ exec { 'apt-update':                    # exec resource named 'apt-update'
 }
 
 # install apache2 package
-package { 'apache2':
-  require => Exec['apt-update'],        # require 'apt-update' before installing
-  ensure => installed,
-}
-
-# ensure apache2 service is running
-service { 'apache2':
-  ensure => running,
-}
-
-# install mysql-server package
-package { 'mysql-server':
-  require => Exec['apt-update'],        # require 'apt-update' before installing
-  ensure => installed,
-}
-
-# ensure mysql service is running
-service { 'mysql':
-  ensure => running,
-}
-
-# ensure info.php file exists
-file { '/var/www/html/index.html':
-  ensure => file,
-  content => '<html><body>Hello world</body></html>',
-  require => Package['apache2'],        # require 'apache2' package before creating
-}
+#package { 'apache2':
+#  require => Exec['apt-update'],        # require 'apt-update' before installing
+#  ensure => installed,
+#}
+#
+## ensure apache2 service is running
+#service { 'apache2':
+#  ensure => running,
+#}
+#
+## install mysql-server package
+#package { 'mysql-server':
+#  require => Exec['apt-update'],        # require 'apt-update' before installing
+#  ensure => installed,
+#}
+#
+## ensure mysql service is running
+#service { 'mysql':
+#  ensure => running,
+#}
+#
+## ensure info.php file exists
+#file { '/var/www/html/index.html':
+#  ensure => file,
+#  content => '<html><body>Hello world</body></html>',
+#  require => Package['apache2'],        # require 'apache2' package before creating
+#}COmmenting out everything except
